d8cd5d7f5c7f4a5e29018f9c7847d5875852dde8
filip-z <filip@zastavnik.be>
2021-03-29 13:34:15 +0200

    Replaced Math file with MathDemoSrc dir

---
+++ MathDemoSrc/Add.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathDemo
+{
+    public class Add
+    {
+        private int a;
+        private int b;
+
+        public Add(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+
+        }
+
+        public int Run()
+        {
+            return a + b;
+        }
+    }
+}
---
+++ MathDemoSrc/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MathDemo
+{
+    public class Math
+    {
+        public int Add(int a, int b)
+        {
+            var add = new Add(a, b);
+            return add.Run();
+        }
+
+        public int Subtract(int a, int b)
+        {
+            var subtract = new Subtract(a, b);
+            return subtract.Run();
+        }
+
+        public ulong Factorial(int n)
+        {
+            int i;
+            ulong factorial = 1;
+
+            for (i = 1; i <= n; i++)
+            {
+                factorial = factorial * (ulong)i;
+            }
+            return factorial;
+        }
+    }
+}
